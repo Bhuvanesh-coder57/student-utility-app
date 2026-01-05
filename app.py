9e57e447ea03a53bf943115e1c6e5e99009d8777
Bhuvanesh S <sbhuvanesh7777@gmail.com>
2026-01-05 23:02:10 +0530

    Add student form with in-memory storage

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+students = []  # Temporary in-memory storage
+
 @app.route('/')
 def home():
-    return " welcome to my Student Utility App "
-@app.route('/about')
-def about():
-    return "This app helps students manage academic data"
+    return render_template('home.html')
 
+@app.route('/add-student', methods=['POST'])
+def add_student():
+    name = request.form['name']
+    marks = request.form['marks']
+    students.append({'name': name, 'marks': marks})
+    return f"Student {name} added with marks {marks}!<br><a href='/'>Go back</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
